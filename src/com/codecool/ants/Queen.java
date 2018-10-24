e433e20a0364f6d3fdfd52a4ac542906bf445dfe
kinga00099 <kinga00099@gmail.com>
2018-10-24 14:14:08 +0200

    Queen class prepared

---
+++ src/com/codecool/ants/Queen.java
@@ -0,0 +1,18 @@
+package com.codecool.ants;
+
+public class Queen extends Ants {
+    private int timesteps;
+
+    //TODO
+    void move(){}
+
+    //TODO
+    boolean wantToMate(){
+        return true;
+    }
+
+    //TODO
+    int setCountDown(){
+        return 0;
+    }
+}
