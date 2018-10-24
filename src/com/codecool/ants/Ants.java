405c184e0b0daff1a10c45ff6d020dc40c57c990
kinga00099 <kinga00099@gmail.com>
2018-10-24 14:07:13 +0200

    Ants class prepared

---
+++ src/com/codecool/ants/Ants.java
@@ -0,0 +1,13 @@
+package com.codecool.ants;
+
+abstract class Ants {
+    private int posX;
+    private int posY;
+
+    abstract void move();
+
+    //TODO
+    public int distanceToQueen(){
+        return 0;
+    }
+}
