cac3da24c3204798afa99a91acf777e91264bfe7
kinga00099 <kinga00099@gmail.com>
2018-10-24 14:26:24 +0200

    Drone/Soldier class extended

--- src/com/codecool/ants/Drone.java
+++ src/com/codecool/ants/Drone.java
@@ -3,4 +3,5 @@ package com.codecool.ants;
 public class Drone extends Ants {
 
     void move(){}
+    void mate(){}
 }
--- src/com/codecool/ants/Soldier.java
+++ src/com/codecool/ants/Soldier.java
@@ -1,6 +1,6 @@
 package com.codecool.ants;
 
 public class Soldier extends Ants {
-
+    private int cycleStep = 0;
     void move(){}
 }
